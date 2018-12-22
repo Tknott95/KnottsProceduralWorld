9f8680a852ff0b05e1f088fb2762bdaca734ba93
Trevor Knott <tknott95@hotmail.com>
2018-12-22 05:56:40 -0700

    IEnumerator to gen each after 1sec for visualization - Genetic Algo Next

--- Assets/Scripts/theGenerator.cs
+++ Assets/Scripts/theGenerator.cs
@@ -9,31 +9,7 @@ public class theGenerator : MonoBehaviour {
 	float spacer = 10;
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(Generate());
-		// float seed = Random.Range(0,100); // reads perlin noise at random points for new gens yo
-		// for(float h = 0; h < mapWidth; h++) {
-		// 	for(float w = 0; w < mapWidth; w++) {
-                
-        //         int perlinNoise = (int) (Mathf.PerlinNoise(w/10.0f + seed,h/10.0f + seed) * 10);
-		// 		Vector3 pos = new Vector3(w * spacer,0,h * spacer);
-		// 		// int n = Random.Range(0, buildings.Length);
-		// 		// Instantiate(buildings[n], pos, Quaternion.identity);
-
-		// 		if(perlinNoise < 2) {
-		// 			Instantiate(buildings[0], pos, Quaternion.identity);
-		// 		} else if (perlinNoise < 4) {
-		// 			Instantiate(buildings[1], pos, Quaternion.identity);
-		// 		} else if (perlinNoise < 6) {
-		// 			Instantiate(buildings[2], pos, Quaternion.identity);
-		// 		} else if (perlinNoise < 8) {
-		// 			Instantiate(buildings[3], pos, Quaternion.identity);
-		// 		} else if (perlinNoise < 10) {
-		// 			Instantiate(buildings[4], pos, Quaternion.identity);
-		// 		}
-		// 		// yield return new WaitForSeconds(1);
-		// 	}
-		// }
-		
+		StartCoroutine(Generate());		
 	}
 
 
@@ -41,7 +17,7 @@ public class theGenerator : MonoBehaviour {
 		float seed = Random.Range(0,100); // reads perlin noise at random points for new gens yo
 		for(float h = 0; h < mapWidth; h++) {
 			for(float w = 0; w < mapWidth; w++) {
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(1/2);
                 int perlinNoise = (int) (Mathf.PerlinNoise(w/10.0f + seed,h/10.0f + seed) * 10);
 				Vector3 pos = new Vector3(w * spacer,0,h * spacer);
 				// int n = Random.Range(0, buildings.Length);
