e707ed7c1e93c104e9b8b29055bd8ca674ef7250
Trevor Knott <tknott95@hotmail.com>
2018-12-22 05:50:20 -0700

    IEnumerator to gen each after 1sec for visualization - Genetic Algo Next

--- Assets/Scripts/theGenerator.cs
+++ Assets/Scripts/theGenerator.cs
@@ -9,10 +9,40 @@ public class theGenerator : MonoBehaviour {
 	float spacer = 10;
 	// Use this for initialization
 	void Start () {
+		StartCoroutine(Generate());
+		// float seed = Random.Range(0,100); // reads perlin noise at random points for new gens yo
+		// for(float h = 0; h < mapWidth; h++) {
+		// 	for(float w = 0; w < mapWidth; w++) {
+                
+        //         int perlinNoise = (int) (Mathf.PerlinNoise(w/10.0f + seed,h/10.0f + seed) * 10);
+		// 		Vector3 pos = new Vector3(w * spacer,0,h * spacer);
+		// 		// int n = Random.Range(0, buildings.Length);
+		// 		// Instantiate(buildings[n], pos, Quaternion.identity);
+
+		// 		if(perlinNoise < 2) {
+		// 			Instantiate(buildings[0], pos, Quaternion.identity);
+		// 		} else if (perlinNoise < 4) {
+		// 			Instantiate(buildings[1], pos, Quaternion.identity);
+		// 		} else if (perlinNoise < 6) {
+		// 			Instantiate(buildings[2], pos, Quaternion.identity);
+		// 		} else if (perlinNoise < 8) {
+		// 			Instantiate(buildings[3], pos, Quaternion.identity);
+		// 		} else if (perlinNoise < 10) {
+		// 			Instantiate(buildings[4], pos, Quaternion.identity);
+		// 		}
+		// 		// yield return new WaitForSeconds(1);
+		// 	}
+		// }
+		
+	}
+
+
+	 IEnumerator Generate() {
 		float seed = Random.Range(0,100); // reads perlin noise at random points for new gens yo
 		for(float h = 0; h < mapWidth; h++) {
 			for(float w = 0; w < mapWidth; w++) {
-				int perlinNoise = (int) (Mathf.PerlinNoise(w/10.0f + seed,h/10.0f + seed) * 10);
+                yield return new WaitForSeconds(1);
+                int perlinNoise = (int) (Mathf.PerlinNoise(w/10.0f + seed,h/10.0f + seed) * 10);
 				Vector3 pos = new Vector3(w * spacer,0,h * spacer);
 				// int n = Random.Range(0, buildings.Length);
 				// Instantiate(buildings[n], pos, Quaternion.identity);
@@ -28,10 +58,10 @@ public class theGenerator : MonoBehaviour {
 				} else if (perlinNoise < 10) {
 					Instantiate(buildings[4], pos, Quaternion.identity);
 				}
+				// yield return new WaitForSeconds(1);
 			}
 		}
-		
-	}
+	 }
 	
 	
 	// // Update is called once per frame
